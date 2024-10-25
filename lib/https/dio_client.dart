e74718dbf3aaf0c576eceb73046958bbf08ac013
xiaoyongkang <1372659241@qq.com>
2024-10-25 11:04:17 +0800

    ~~

--- lib/https/dio_client.dart
+++ lib/https/dio_client.dart
@@ -1,10 +1,22 @@
 import 'package:dio/dio.dart';
-const String BASE_URL = 'https://de1.api.radio-browser.info/';
+const String baseUrl = 'https://de1.api.radio-browser.info/';
+const int connectTimeout = 5000;
+const int receiveTimeout = 5000;
+
 class DioClient{
   Dio? dio;
   static final DioClient _instance = DioClient._internal();
   factory DioClient() => _instance;
   DioClient._internal(){
-
+    dio ??= Dio(
+        BaseOptions(
+          baseUrl: baseUrl,
+          connectTimeout: const Duration(milliseconds: connectTimeout),
+          receiveTimeout: const Duration(milliseconds: receiveTimeout)
+        )
+      );
   }
+  // static Future<void> requestData() async{
+  //
+  // }
 }
